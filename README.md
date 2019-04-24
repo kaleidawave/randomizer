7b802142fd0f8b436ea96fe11c8f8b4063fee092
kaleidagraph <26967284+kaleidagraph@users.noreply.github.com>
2019-04-24 20:27:53 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# randomizer
+<h1 align=center>Randomizer</h1>
+
 A randomizer made in C# for choosing a random item with a weighted chance
+
+<h2 align=center>Usage:</h2>
+
+```cs
+using Randomizer;
+
+// Create an object that inherits the 'IWeighted' inheritance with the 'Weight' property
+struct Reward: IWeighted
+{
+    int Weight { get; set; }
+    ...
+}
+
+// Past a list of objects into the constructor
+List<Reward> foo = ...
+Randomizer<Reward> bar = new Randomizer<Reward>(foo);
+
+// Retrieve results!!!
+Reward x = bar.Pick();
+```
---
+++ Randomizer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Randomizer
+{
+    /// <summary>
+    /// A class to pick random items from a list.
+    /// </summary>
+    /// <see>
+    /// For usage see: https://github.com/kaleidawave/randomizer
+    /// </see>
+    public class Randomizer<T> where T : IWeighted
+    {
+        private IList<T> _entries;
+        private Random _random;
+        private int _totalWeight;
+
+        public Randomizer(List<T> entries)
+        {
+            this._entries = entries;
+            this._random = new Random();
+            this._totalWeight = entries.Sum(x => x.Weight);
+        }
+
+        /// <summary>
+        /// Returns a random item from the collection
+        /// </summary>
+        /// <remarks>Items with a greater weight will appear more frequent</remarks>
+        public T Pick()
+        {
+            int picked = this._random.Next(0, this._totalWeight) + 1;
+            foreach (T item in this._entries)
+            {
+                picked -= item.Weight;
+                if (picked <= 0) return item;
+            }
+            throw new Exception("Outside of range"); // This should not throw
+        }
+    }
+
+    /// <summary>
+    /// Object has a weight for when picked by a tombolo
+    /// </summary>
+    public interface IWeighted
+    {
+        int Weight { get; set; }
+    }
+}
